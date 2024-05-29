08841951b634b6f4740725a1656c7e2aa573157d
Dominika Nowak <dominika.nowak@student.put.poznan.pl>
2024-05-30 00:45:22 +0200

    Zad 1. Zamiana modelu wypełnionego na siatkę modelu

--- g_simplest.glsl
+++ g_simplest.glsl
@@ -1,20 +1,26 @@
 #version 330
 
 layout(triangles) in;
-layout(triangle_strip) out;
-layout(max_vertices = 3) out;
+layout(line_strip, max_vertices = 6) out;
 
 in vec4 gColor[];
 out vec4 iColor;
 
-void main(void) {
-	int i;
-
-	for (i = 0; i < gl_in.length(); i++) {
-		gl_Position = gl_in[i].gl_Position;
+void main() {
+    // Pêtla iteruj¹ca po trójk¹cie wejœciowym
+    for (int i = 0; i < 3; ++i) {
+        // Przekazywanie pozycji i koloru punktu
+        gl_Position = gl_in[i].gl_Position;
         iColor = gColor[i];
         EmitVertex();
 
-	}
-	EndPrimitive();
+        // Nastêpny wierzcho³ek tworz¹cy odcinek
+        int nextIndex = (i + 1) % 3;
+        gl_Position = gl_in[nextIndex].gl_Position;
+        iColor = gColor[nextIndex];
+        EmitVertex();
+
+        // Koniec odcinka
+        EndPrimitive();
+    }
 }
