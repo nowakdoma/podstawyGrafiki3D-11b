b95724ef6cc3c58618b3d20fdc46c8e2fd962d53
Dominika Nowak <dominika.nowak@student.put.poznan.pl>
2024-05-30 00:46:57 +0200

    Zad 2. Zrobienie "koronkowej" siatki modelu.

--- g_simplest.glsl
+++ g_simplest.glsl
@@ -9,15 +9,22 @@ out vec4 iColor;
 void main() {
     // Pêtla iteruj¹ca po trójk¹cie wejœciowym
     for (int i = 0; i < 3; ++i) {
-        // Przekazywanie pozycji i koloru punktu
+        // Przekazywanie pozycji i koloru punktu A
         gl_Position = gl_in[i].gl_Position;
         iColor = gColor[i];
         EmitVertex();
 
-        // Nastêpny wierzcho³ek tworz¹cy odcinek
-        int nextIndex = (i + 1) % 3;
-        gl_Position = gl_in[nextIndex].gl_Position;
-        iColor = gColor[nextIndex];
+        // Przekazywanie pozycji i koloru punktu D (œrednia z A, B i C)
+        int nextIndex1 = (i + 1) % 3;
+        int nextIndex2 = (i + 2) % 3;
+        gl_Position = (gl_in[i].gl_Position + gl_in[nextIndex1].gl_Position + gl_in[nextIndex2].gl_Position) / 3.0;
+        iColor = (gColor[i] + gColor[nextIndex1] + gColor[nextIndex2]) / 3.0;
+        EmitVertex();
+
+        // Nastêpny wierzcho³ek tworz¹cy odcinek (punkt B lub C)
+        int nextIndex3 = (i + 2) % 3;
+        gl_Position = gl_in[nextIndex3].gl_Position;
+        iColor = gColor[nextIndex3];
         EmitVertex();
 
         // Koniec odcinka
